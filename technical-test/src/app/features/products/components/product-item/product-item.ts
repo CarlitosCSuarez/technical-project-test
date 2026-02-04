c591e1438e421915f92d826f1b9c8feccad3a387
Carlitcs CS <carlitcscs@Carlitcss-MacBook-Pro.local>
2026-02-04 09:11:43 -0500

    Formatting product dates

--- technical-test/src/app/features/products/components/product-item/product-item.ts
+++ technical-test/src/app/features/products/components/product-item/product-item.ts
@@ -1,11 +1,13 @@
 import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
 import { Product } from '../../../../core/models/product';
 import { RouterLink } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-item',
   imports: [
     // RouterLink,
+    DatePipe,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './product-item.html',
