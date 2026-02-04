1a531c1f123062d75212cfe48b971fd5d4023b5b
Carlitcs CS <carlitcscs@Carlitcss-MacBook-Pro.local>
2026-02-03 23:51:30 -0500

    Add nav-bar partially, styles partially about button, select, modal, product-list, update colors, update app

--- technical-test/src/app/shared/components/button/button.ts
+++ technical-test/src/app/shared/components/button/button.ts
@@ -1,14 +1,19 @@
+import { NgClass, NgStyle } from '@angular/common';
 import { Component, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
-  imports: [],
+  imports: [
+    NgClass,
+    NgStyle,
+  ],
   templateUrl: './button.html',
   styleUrl: './button.scss',
 })
 export class Button {
 
   label = input.required<string>();
+  maxWith = input<string>('max-content');
   option = input<'primary' | 'secondary'>('primary');
   disabled = input<boolean>(false);
 
